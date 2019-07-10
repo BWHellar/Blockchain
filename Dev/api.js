900af5e95311feb1f1783c8c2c24695a578c51c7
bh <bwhellar@gmail.com>
2019-07-10 15:29:30 -0700

    finish the post transaction functionality

--- Dev/api.js
+++ Dev/api.js
@@ -9,19 +9,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 // First Endpoint
+// We dont need much to send our named hash
 app.get('/blockchain', function(req, res){
   res.send(BrenCoin);
 });
 
 //Second Endpoint
 app.post('/transaction', function(req, res){
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} Bitcoin`);
+  const blockIndex = BrenCoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: `Transaction will be added in block ${blockIndex}`});
 });
 
 //Third Endpoint
 app.get('/mine', function(req, res){
-  
+  const newBlock = BrenCoin.createNewBlock();
 });
 // So we know that the port is listening of pass data.
 app.listen(3000, function(){
