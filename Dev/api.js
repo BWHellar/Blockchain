af636acef36c783c30977f20c20d54a5454311f3
bh <bwhellar@gmail.com>
2019-07-10 14:54:13 -0700

    fixed minor Postman issues

--- Dev/api.js
+++ Dev/api.js
@@ -1,8 +1,24 @@
-var express = require ('express') // We want to have all our endpoints built out by ExpressJS
-var app = express()
+const express = require ('express'); // We want to have all our endpoints built out by ExpressJS
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res){
-  res.send('Hello Bruh')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+// First Endpoint
+app.get('/blockchain', function (req, res){
+  
+});
 
-app.listen(3000)
+//Second Endpoint
+app.post('/transaction', function(req, res){
+  res.send(`The amount of the transaction is ${req.body.amount} Bitcoin`);
+});
+
+//Third Endpoint
+app.get('/mine', function(req, res){
+  
+});
+// So we know that the port is listening of pass data.
+app.listen(5558, function(){
+  console.log('Listening on port 5558');
+});
