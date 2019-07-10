9b704619ed88aa7e6c5e37eadd234d70885e81e6
bh <bwhellar@gmail.com>
2019-07-10 15:07:24 -0700

    fix issue where hash info was not showing

--- Dev/api.js
+++ Dev/api.js
@@ -1,16 +1,21 @@
 const express = require ('express'); // We want to have all our endpoints built out by ExpressJS
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const BrenCoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
 // First Endpoint
-app.get('/blockchain', function (req, res){
-  
+app.get('/blockchain', function(req, res){
+  res.send(BrenCoin);
 });
 
 //Second Endpoint
 app.post('/transaction', function(req, res){
+  console.log(req.body);
   res.send(`The amount of the transaction is ${req.body.amount} Bitcoin`);
 });
 
@@ -19,6 +24,6 @@ app.get('/mine', function(req, res){
   
 });
 // So we know that the port is listening of pass data.
-app.listen(5558, function(){
-  console.log('Listening on port 5558');
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
 });
